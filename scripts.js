915bb00f95b622ccbabbf3290dc730473ac1b5e4
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-07-25 16:53:35 -0400

    Add game logic

--- scripts.js
+++ scripts.js
@@ -1,6 +1,26 @@
 let gameChoices = ["rock", "paper", "scissors"];
 let randItem = gameChoices[Math.floor(Math.random() * 3)];
 let input = prompt("Please enter ('rock', 'paper' or 'scissors'): ").toLowerCase(); // Checks for case sensitivity.
+console.log(`You chose: ${input}\nCPU chose: ${randItem}`)
 
-
-alert(input);
+if (input === "rock" && randItem === "paper") {
+    console.log(`${randItem} wins!`);
+}
+else if (input === "rock" && randItem === "scissors") {
+    console.log(`${input} wins!`);
+}
+else if (input === "paper" && randItem === "scissors") {
+    console.log(`${randItem} wins!`);
+}
+else if (input === "paper" && randItem === "rock") {
+    console.log(`${input} wins!`);
+}
+else if (input === "scissors" && randItem === "rock") {
+    console.log(`${randItem} wins!`);
+}
+else if (input === "scissors" && randItem === "paper") {
+    console.log(`${input} wins!`);
+}
+else {
+    console.log(`No winner!`);
+}
