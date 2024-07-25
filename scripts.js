2a40b264cf1b4d604fc096cae34c6d7339c0bb9c
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-07-25 17:03:00 -0400

    Re-named variables

--- scripts.js
+++ scripts.js
@@ -1,25 +1,25 @@
 let gameChoices = ["rock", "paper", "scissors"];
-let randItem = gameChoices[Math.floor(Math.random() * 3)];
-let input = prompt("Please enter ('rock', 'paper' or 'scissors'): ").toLowerCase(); // Checks for case sensitivity.
-console.log(`You chose: ${input}\nCPU chose: ${randItem}`)
+let getComputerChoice = gameChoices[Math.floor(Math.random() * 3)];
+let getHumanChoice = prompt("Please enter ('rock', 'paper' or 'scissors'): ").toLowerCase(); // Checks for case sensitivity.
+console.log(`You chose: ${getHumanChoice}\nCPU chose: ${getComputerChoice}`)
 
-if (input === "rock" && randItem === "paper") {
-    console.log(`${randItem} wins!`);
+if (getHumanChoice === "rock" && getComputerChoice === "paper") {
+    console.log(`${getComputerChoice} wins!`);
 }
-else if (input === "rock" && randItem === "scissors") {
-    console.log(`${input} wins!`);
+else if (getHumanChoice === "rock" && getComputerChoice === "scissors") {
+    console.log(`${getHumanChoice} wins!`);
 }
-else if (input === "paper" && randItem === "scissors") {
-    console.log(`${randItem} wins!`);
+else if (getHumanChoice === "paper" && getComputerChoice === "scissors") {
+    console.log(`${getComputerChoice} wins!`);
 }
-else if (input === "paper" && randItem === "rock") {
-    console.log(`${input} wins!`);
+else if (getHumanChoice === "paper" && getComputerChoice === "rock") {
+    console.log(`${getHumanChoice} wins!`);
 }
-else if (input === "scissors" && randItem === "rock") {
-    console.log(`${randItem} wins!`);
+else if (getHumanChoice === "scissors" && getComputerChoice === "rock") {
+    console.log(`${getComputerChoice} wins!`);
 }
-else if (input === "scissors" && randItem === "paper") {
-    console.log(`${input} wins!`);
+else if (getHumanChoice === "scissors" && getComputerChoice === "paper") {
+    console.log(`${getHumanChoice} wins!`);
 }
 else {
     console.log(`No winner!`);
