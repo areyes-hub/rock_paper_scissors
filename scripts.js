8118cf611af9fb58e5f934fc16cba2d9ada7b59c
areyes-hub <68817338+areyes-hub@users.noreply.github.com>
2024-07-26 11:02:48 -0400

    Implement playRound and playGame functions

--- scripts.js
+++ scripts.js
@@ -1,26 +1,74 @@
 let gameChoices = ["rock", "paper", "scissors"];
-let getComputerChoice = gameChoices[Math.floor(Math.random() * 3)];
-let getHumanChoice = prompt("Please enter ('rock', 'paper' or 'scissors'): ").toLowerCase(); // Checks for case sensitivity.
-console.log(`You chose: ${getHumanChoice}\nCPU chose: ${getComputerChoice}`)
+let humanScore = 0;
+let cpuScore = 0;
 
-if (getHumanChoice === "rock" && getComputerChoice === "paper") {
-    console.log(`${getComputerChoice} wins!`);
+function keepScore(p1, p2) {
+    console.log(`Your score: ${p1}\nCPU score: ${p2}`);
 }
-else if (getHumanChoice === "rock" && getComputerChoice === "scissors") {
-    console.log(`${getHumanChoice} wins!`);
+
+function getComputerChoice() {
+    let getCPUChoice = gameChoices[Math.floor(Math.random() * 3)];
+    return getCPUChoice;
 }
-else if (getHumanChoice === "paper" && getComputerChoice === "scissors") {
-    console.log(`${getComputerChoice} wins!`);
+
+function getHumanChoice() {
+    let getChoice = prompt("To play, please enter ('rock', 'paper' or 'scissors'): ").toLowerCase(); // Checks for case sensitivity.
+    return getChoice;
 }
-else if (getHumanChoice === "paper" && getComputerChoice === "rock") {
-    console.log(`${getHumanChoice} wins!`);
+
+function playRound(humanChoice, computerChoice) {
+
+    console.log(`You chose: ${humanChoice}\nCPU chose: ${computerChoice}`);
+
+    if (humanChoice === "rock" && computerChoice === "paper") {
+        console.log(`${computerChoice} wins!`);
+        cpuScore += 1;
+    }
+    else if (humanChoice === "rock" && computerChoice === "scissors") {
+        console.log(`${humanChoice} wins!`);
+        humanScore += 1;
+    }
+    else if (humanChoice === "paper" && computerChoice === "scissors") {
+        console.log(`${computerChoice} wins!`);
+        cpuScore += 1;
+    }
+    else if (humanChoice === "paper" && computerChoice === "rock") {
+        console.log(`${humanChoice} wins!`);
+        humanScore += 1;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "rock") {
+        console.log(`${computerChoice} wins!`);
+        cpuScore += 1;
+    }
+    else if (humanChoice === "scissors" && computerChoice === "paper") {
+        console.log(`${humanChoice} wins!`);
+        humanScore += 1;
+    }
+    else {
+        console.log(`No winner!`);
+    }
 }
-else if (getHumanChoice === "scissors" && getComputerChoice === "rock") {
-    console.log(`${getComputerChoice} wins!`);
+
+function playGame() {
+    let humanSelection = getHumanChoice();
+    let compSelection = getComputerChoice();
+
+    playRound(humanSelection, compSelection);
+    keepScore(humanScore, cpuScore);
+
+    let user = prompt("Do you want to play again? ").toLowerCase();
+    if (user === "yes") {
+        playGame();
+    }
+    else if (user === "no") {
+        return 0;
+    }
+    else {
+        console.log("Input not recognized...");
+        return 0;
+    }
 }
-else if (getHumanChoice === "scissors" && getComputerChoice === "paper") {
-    console.log(`${getHumanChoice} wins!`);
-}
-else {
-    console.log(`No winner!`);
-}
+
+playGame();
+
+
